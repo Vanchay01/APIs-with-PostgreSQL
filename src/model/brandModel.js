392670873ecc1dfaaf485746954b9d3e3fe40676
chayozo <chaydouble0@gmail.com>
2025-12-29 20:04:46 +0700

    Created brandModelo{find, findOne, deleteOne, and updateOne}

--- src/model/brandModel.js
+++ src/model/brandModel.js
@@ -2,10 +2,23 @@ const pool = require('../config/db')
 
 const brandModel = {
     async save({name}){
-        const brand = await pool.query(`
-            Insert into brands(name)
-            Values($1) Returning *
-        `, [name])
+        const brand = await pool.query(`Insert into brands(name) Values($1) returning *`, [name])
+        return brand.rows[0]
+    },
+    async find(){
+        const brand = await pool.query("Select * from brands returning *")
+        return brand.rows[0]
+    },
+    async findOne({id}){
+        const brand = await pool.query(`Select * from brands where id = $1 returning *`, [id])
+        return brand.rows[0]
+    },
+    async deleteOne({id}){
+        const brand = await pool.query(`Delete from brands where id = $1 returning *`, [id])
+        return brand.rows[0]
+    },
+    async updateOne({id, name}){
+        const brand = await pool.query(`Update brands set name = $1 where id $2 returning *`, [id, name])
         return brand.rows[0]
     }
 }
