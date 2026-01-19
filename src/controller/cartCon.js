7633fa0c7cad8e853cb1e3f0afc7d8c0e2b24422
Vanchay <102397819+Vanchay01@users.noreply.github.com>
2026-01-19 22:24:09 +0700

    Create new carts

--- src/controller/cartCon.js
+++ src/controller/cartCon.js
@@ -3,11 +3,21 @@ const cartModel = require("../model/cartModel")
 
 const addCard = asyncHandler(async(req, res)=>{
     const {byUser, status} = req.body
-    const exsisting = cartModel.findCard({byUser: byUser, status: status})
-    if(!exsisting){
-        return res.json("Not found exsisting")
+    const exsisting = await cartModel.findCard({byUser: byUser, status: status})
+    console.log("controller", exsisting)
+    if(exsisting){
+        return res.json({
+            message: "Cart created already",
+            status: 200,
+        })
     }
-    return res.json("exsisting")
+    console.log("===>controller", exsisting)
+    const result = await cartModel.save({byUser: byUser, status: status})
+    return res.json({
+        message: "Added new carts",
+        status: 200,
+        data: result
+    })
 
 })
 module.exports = {addCard}
--- src/model/cartModel.js
+++ src/model/cartModel.js
@@ -2,15 +2,20 @@ const pool = require("../config/db")
 
 const cartModel = {
     async findCard({byUser, status}){
+        console.log(byUser, status)
         const sql = await pool.query('SELECT * FROM carts WHERE by_users = $1 AND status = $2', [byUser, status])
+        console.log("Model", sql.rows)
         if(sql.rows.length){
-            return result.rows.id;
+           return sql.rows;
         }
     },
     async save({byUser, status}){
-        const sql = await pool.query(`INSERT INTO carts(byUser, status) Values($1, $2) returning *`, [byUser, status])
+        const sql = await pool.query(`INSERT INTO carts(by_users, status) Values($1, $2) returning *`, [byUser, status])
+        console.log(sql.rows)
         return sql.rows
     },
+    
 }
+
 
 module.exports = cartModel
